8e9a181f9c4552b2ac2d60d0671a1f7c97be7c60
MiaRene15 <kids.hines@icloud.com>
2018-06-22 21:14:55 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -7,11 +7,10 @@ let operator = $('#operation')
 
 
 function validate(operator, numOne, numTwo) {
-  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') { if (isNaN(numOne) || numOne === '' || isNaN(numTwo) || numTwo === '' ) { $('#result').text("Sorry, one of those is not a valid number!")
-    
+  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') { if (isNaN(numOne) || numOne === '' || isNaN(numTwo) || numTwo === '' ) { $('#result').text("Sorry, one of those is not a valid number!");} else { answer(operator, numOne, numTwo);}
+  
+  
   }
-  
-  
   
 }
 
